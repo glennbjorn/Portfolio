9e902ec8bb28b70227621b2076077a918b63e33d
glennbjorn <77476786+glennbjorn@users.noreply.github.com>
2022-06-21 21:41:39 +0800

    edited index.css

--- glennbjorn-portfolio-website/src/App.jsx
+++ glennbjorn-portfolio-website/src/App.jsx
@@ -3,20 +3,20 @@ import Header from "./components/header/Header";
 import Nav from "./components/nav/Nav";
 import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
-import Contact from "./components/contacts/Contacts";
+import Contacts from "./components/contacts/Contacts";
 import Footer from "./components/footer/Footer";
 
 const App = () => {
-    return (
-        <>
-            <Header />
-            <Nav />
-            <About />
-            <Experience />
-            <Contact />
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Contacts />
+      <Footer />
+    </>
+  );
 };
 
 export default App;

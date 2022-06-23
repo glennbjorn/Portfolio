f649b2bc138064e76bf6fca2dd40f91cfec54245
glennbjorn <77476786+glennbjorn@users.noreply.github.com>
2022-06-23 15:48:53 +0800

    updated About Me

--- glennbjorn-portfolio-website/src/components/about/About.jsx
+++ glennbjorn-portfolio-website/src/components/about/About.jsx
@@ -1,10 +1,60 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
+import AboutImg from "../../assets/about-img.jpg";
+import { GiTiedScroll } from "react-icons/gi";
+import { GiStairsGoal } from "react-icons/gi";
+import { VscFolderLibrary } from "react-icons/vsc";
 
 const About = () => {
   return (
-    <section id='about'>About</section>
-  )
-}
+    <section id="about">
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
 
-export default About
+      <div className="container about__container">
+        <div className="about__me">
+          <div className="about__me-image">
+            <img src={AboutImg} alt="About Image" />
+          </div>
+        </div>
+
+        <div className="about__content">
+          <div className="about__cards">
+            <article className="about__card">
+              <GiTiedScroll className="about__icon" />
+              <h5>Education</h5>
+              <small>Year 3 NUS Business Analytics</small>
+            </article>
+            <article className="about__card">
+              <GiStairsGoal className="about__icon" />
+              <h5>Mission</h5>
+              <small>Empower the differently-abled</small>
+            </article>
+            <article className="about__card">
+              <VscFolderLibrary className="about__icon" />
+              <h5>Projects</h5>
+              <small>2 completed</small>
+            </article>
+          </div>
+
+          <p>
+            I am passionate about making a difference using technology. I
+            consider myself driven and competitive, but I also champion for
+            mental wellness and discourage people from burning out. I am
+            meticulous and task-oriented. In a team project, I play the role of
+            a risk-evaluator more than a creative thinker, though I am also
+            strong at problem-solving. Outside of work, I am a caring individual
+            who will look out for my peers and colleagues and be their listening
+            ear.
+          </p>
+
+          <a href="#contacts" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;

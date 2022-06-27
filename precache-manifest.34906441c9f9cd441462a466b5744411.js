3c3c092e8a0cd12e7e9a9e98a2dcf199916d307f
glennbjorn <77476786+glennbjorn@users.noreply.github.com>
2022-06-27 11:21:40 +0800

    Updates

---
+++ precache-manifest.34906441c9f9cd441462a466b5744411.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6cd34d19e4d6459b12f",
+    "url": "/Portfolio/static/css/main.f2361cd6.chunk.css"
+  },
+  {
+    "revision": "b6cd34d19e4d6459b12f",
+    "url": "/Portfolio/static/js/main.b6cd34d1.chunk.js"
+  },
+  {
+    "revision": "41b012d2b8f03da591f0",
+    "url": "/Portfolio/static/js/1.41b012d2.chunk.js"
+  },
+  {
+    "revision": "3b96116072eb85f5a3a2",
+    "url": "/Portfolio/static/js/runtime~main.3b961160.js"
+  },
+  {
+    "revision": "3d4eb8022b2202ad85e1f661361e7506",
+    "url": "/Portfolio/static/media/CV.3d4eb802.pdf"
+  },
+  {
+    "revision": "92b812d4a67c8eaccb0b2b7371ad9d68",
+    "url": "/Portfolio/static/media/dp.92b812d4.png"
+  },
+  {
+    "revision": "ad0d400e0d99ca453fd5b9d5f17ff03f",
+    "url": "/Portfolio/static/media/about-img.ad0d400e.jpg"
+  },
+  {
+    "revision": "05a667a5cc7650eef4109640d092f7be",
+    "url": "/Portfolio/static/media/bg-texture.05a667a5.jpg"
+  },
+  {
+    "revision": "222a009c0ea6ac58896fe810942fe369",
+    "url": "/Portfolio/index.html"
+  }
+];
